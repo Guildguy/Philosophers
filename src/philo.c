3b0b90482ba35061113ab883a36c91c25a6537d0
Guildguy <fabiolljunior23@gmail.com>
2025-05-02 14:35:32 -0300

    creating the basic structure to the main file in project

---
+++ src/philo.c
@@ -0,0 +1,15 @@
+#include "philo.h"
+int	main(int c, char **v)
+{
+	if (c < 5 || c > 6)
+	{
+		printf(MSG);
+		//num. filos = qnt de filos e garfo
+		//tmp. morte = em milisegundos, o filo precisa comer dentro do tempo imposto em <tempoMorte>, se expirar, ele morre
+		//tmp. comer = em milisegundo, o tempo que leva para o filo comer, ele precisa segurar os 2 garfos
+		//tmp. mimir = em milisegundos, o tempo que o filo gasta dormindo
+		// n x cada filo pracisa comer = (opcional), se for imposto, a simulação acaba 
+		  //quando o filo comer as x vezes impostas, se não utilizado, acaba quando todos morrem
+	}
+	return (0);
+}
