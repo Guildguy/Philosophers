f8f1958e493472d49a407f6e7159663a785adcfd
Guildguy <fabiolljunior23@gmail.com>
2025-05-06 23:50:15 -0300

    prototype to modularize and organize the functions in smart chunks

---
+++ src/philosopher_creation.c
@@ -0,0 +1,66 @@
+#include "../philo.h"
+
+void	*philo_creation(void *arg)
+{
+	t_philo	*philo = (t_philo *)arg;
+	printf("philosopher [%d] created!\n", philo->id);
+	return (NULL);
+}
+
+int	init_data(t_data *data, char **v)
+{
+	int	i;
+
+	i = 0;
+	data->nbr_of_philos = atoi(v[1]);
+	if (data->nbr_of_philos <= 0)
+	{
+		printf("Error: must exist at least 1 philosopher!\n");
+		return (1);
+	}
+	data->philos = malloc(sizeof(t_philo) * data->nbr_of_philos);
+	data->threads = malloc(sizeof(pthread_t) * data->nbr_of_philos);
+	if (!data->philos || !data->threads)
+	{
+		printf("Error: failure to alocate memory!\n");
+		free(data->philos);
+		free(data->threads);
+		return (1);
+	}
+	while (i < data->nbr_of_philos)
+	{
+		data->philos[i].id = i + 1;
+		i++;
+	}
+	return (0);
+}
+
+int	philo_create(t_data *data)
+{
+	int	i;
+	i = 0;
+
+	while (i < data->nbr_of_philos)
+	{
+		if (pthread_create(&data->threads[i], NULL, philo_creation, &data->philos[i]) != 0)
+		{
+			printf("Error: failure to create thread n°: [%d]!\n", i + 1);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	philo_wait(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nbr_of_philos)
+	{
+		if (pthread_join(data->threads[i], NULL) != 0)
+			printf("Error: failure in wait the thread [%d]!\n", i + 1);
+		i++;
+	}
+}
