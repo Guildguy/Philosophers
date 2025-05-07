3d346e6dc9be6fade12ee47bc63742a2ea883976
Guildguy <fabiolljunior23@gmail.com>
2025-05-06 22:29:20 -0300

    Created the function to liberate the memory allocated to the philosophers and the threads

--- src/philo.c
+++ src/philo.c
@@ -65,15 +65,14 @@ void	philo_wait(t_data *data)
 	}
 }
 
-
+void	free_philo(t_data *data)
+{
+	free(data->philos);
+	free(data->threads);
+}
 
 int	main(int c, char **v)
 {
-	int			i;
-	int			*IDs;
-	int			nbr_of_philos;
-	pthread_t	*philos;
-
 	if (/*c < 5 || c > 6*/ c != 2)
 	{
 		printf(MSG);
@@ -85,43 +84,6 @@ int	main(int c, char **v)
 		// n x cada filo pracisa comer = (opcional), se for imposto, a simulação acaba 
 		  //quando o filo comer as x vezes impostas, se não utilizado, acaba quando todos morrem
 	}
-	nbr_of_philos = atoi(v[1]);
-	if (nbr_of_philos <= 0)
-	{
-		write(2, "Error: must exist at least 1 philosopher!\n", 42);
-		return (1);
-	}
-	philos = malloc(sizeof(pthread_t) * nbr_of_philos);
-	IDs = malloc(sizeof(int) * nbr_of_philos);
-	if (!philos || !IDs)
-	{
-		write(2, "Error: failure to alocate memory!\n", 34);
-		free(philos);
-		free(IDs);
-		return (1);
-	}
-	i = 0;
-	while (i < nbr_of_philos)
-	{
-		IDs[i] = i + 1;
-		if (pthread_create(&philos[i], NULL, philo_creation, &IDs[i]) != 0)
-		{
-			printf("Error: failure to create thread n°: [%d]!\n", i + 1);
-			free(philos);
-			free(IDs);
-			return (1);
-		}
-		i++;
-	}
 	
-	i = 0;
-	while (i < nbr_of_philos)
-	{
-		if (pthread_join(philos[i], NULL) != 0)
-			printf("Error: failure in wait the thread [%d]!\n", i + 1);
-		i++;
-	}
-	free(philos);
-	free(IDs);
 	return (0);
 }
