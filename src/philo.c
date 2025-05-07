cf1a79269edc38251704060e8724f77234b9ea2e
Guildguy <fabiolljunior23@gmail.com>
2025-05-06 23:47:47 -0300

    separated the philosopher funtion in another file.c to modularize

--- src/philo.c
+++ src/philo.c
@@ -1,76 +1,5 @@
 #include "../philo.h"
 
-void	*philo_convertion(void *arg)
-{
-	t_philo	*philo = (t_philo *)arg;
-	printf("philosopher [%d] created!\n", philo->ID);
-	return (NULL);
-}
-
-int	init_data(t_data *data, char **v)
-{
-	int	i;
-
-	i = 0;
-	data->nbr_of_philos = atoi(v[1]);
-	if (data->nbr_of_philos <= 0)
-	{
-		printf("Error: must exist at least 1 philosopher!\n");
-		return (1);
-	}
-	data->philos = malloc(sizeof(t_philo) * data->nbr_of_philos);
-	data->threads = malloc(sizeof(pthread_t) * data->nbr_of_philos);
-	if (!data->philos || !data->threads)
-	{
-		printf("Error: failure to alocate memory!\n");
-		free(data->philos);
-		free(data->threads);
-		return (1);
-	}
-	while (i < data->nbr_of_philos)
-	{
-		data->philos[i].ID = i + 1;
-		i++;
-	}
-	return (0);
-}
-
-int	philo_create(t_data *data)
-{
-	int	i;
-	i = 0;
-
-	while (i < data->nbr_of_philos)
-	{
-		if (pthread_create(&data->threads[i], NULL, philo_convertion, &data->philos[i]) != 0)
-		{
-			printf("Error: failure to create thread n°: [%d]!\n", i + 1);
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-void	philo_wait(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->nbr_of_philos)
-	{
-		if (pthread_join(data->threads[i], NULL) != 0)
-			printf("Error: failure in wait the thread [%d]!\n", i + 1);
-		i++;
-	}
-}
-
-void	free_philo(t_data *data)
-{
-	free(data->philos);
-	free(data->threads);
-}
-
 int	main(int c, char **v)
 {
 	t_data	data = {0};
@@ -87,12 +16,12 @@ int	main(int c, char **v)
 	}
 	if (init_data(&data, v) != 0)
 		return (1);
-	if (philo_convertion(&data) != 0)
+	if (philo_creation(&data) != 0)
 	{
-		free_philo(&data);
+		free_all(&data);
 		return (1);
 	}
 	philo_wait(&data);
-	free_philo(&data);
+	free_all(&data);
 	return (0);
 }
