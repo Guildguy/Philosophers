e232f63740a1cf3830a849f78593b1e128da8362
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-05-10 17:43:39 -0300

    added verification and freethe mutex to update and modularize the philosopher_creation()

--- src/data_handler.c
+++ src/data_handler.c
@@ -2,11 +2,26 @@
 
 void	free_all(t_data *data)
 {
+	int	i;
+
 	if (data)
 	{
-		free(data->philos);
+		if (data->philos)
+			free(data->philos);
 		data->philos = NULL;
-		free(data->threads);
+		if (data->threads)
+			free(data->threads);
 		data->threads = NULL;
+		if (data->forks)
+		{
+			i = 0;
+			while (i < data->nbr_of_philos)
+			{
+				pthread_mutex_destroy(&data->forks[i]);
+				i++;
+			}
+			free(data->forks);
+		}
+		data->forks = NULL;
 	}
 }
