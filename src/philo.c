a2de889b219cac15e32cd7818c3347996d62bf8a
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-05-10 17:31:15 -0300

    updating a new line :/

--- src/philo.c
+++ src/philo.c
@@ -7,12 +7,6 @@ int	main(int c, char **v)
 	{
 		printf(MSG);
 		return (1);
-		//num. filos = qnt de filos e garfo
-		//tmp. morte = em milisegundos, o filo precisa comer dentro do tempo imposto em <tempoMorte>, se expirar, ele morre
-		//tmp. comer = em milisegundo, o tempo que leva para o filo comer, ele precisa segurar os 2 garfos
-		//tmp. mimir = em milisegundos, o tempo que o filo gasta dormindo
-		// n x cada filo pracisa comer = (opcional), se for imposto, a simulação acaba 
-		  //quando o filo comer as x vezes impostas, se não utilizado, acaba quando todos morrem
 	}
 	if (init_data(&data, v) != 0)
 		return (1);
