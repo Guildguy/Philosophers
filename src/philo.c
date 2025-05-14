2e973fda7b0f181222c37da42eba770c1a846f0d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-05-14 20:33:34 -0300

    changed condition to become more usefull

--- src/philo.c
+++ src/philo.c
@@ -5,12 +5,14 @@ int	main(int c, char **v)
 	t_data	data = {0};
 	if (c != 5 && c != 6)
 	{
-		free_all(&data);
 		printf(MSG);
 		return (1);
 	}
 	if (init_data(&data, c, v) != 0)
+	{
+		free_all(&data);
 		return (1);
+	}
 	if (philo_creation(&data) != 0)
 	{
 		free_all(&data);
