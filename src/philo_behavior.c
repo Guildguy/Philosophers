3b94080cf11f2f89f12a41f36f16640e93ec6024
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-05-17 15:27:45 -0300

    fix(norm errors): replaced spaces with tab and made then in accord to norm

--- src/philo_behavior.c
+++ src/philo_behavior.c
@@ -1,15 +1,26 @@
 #include "../philo.h"
 
-int	*philo_behavior(t_philo *philo, char *action)
+int	philo_behavior(t_philo *philo, char *action)
 {
-    pthread_mutex_lock(&philo->data->print_mutex);
-    printf("Philosopher [%d] %s!\n", philo->id, action);
-    pthread_mutex_unlock(&philo->data->print_mutex);
-    return (0);
+	pthread_mutex_lock(&philo->data->print_mutex);
+	printf("Philosopher [%d] %s!\n", philo->id, action);
+	pthread_mutex_unlock(&philo->data->print_mutex);
+	return (0);
 }
 
-int behavior_prevention(t_philo *philo, unsigned int *l_fork, unsigned int *r_fork)
+int	behavior_prevention(t_philo *philo, unsigned int *l_fork,
+		unsigned int *r_fork)
 {
-    pthread_mutex_lock(&philo->data->dead_mutex);
-    pthread_mutex_unlock(&philo->data->dead_mutex);
+	pthread_mutex_lock(&philo->data->dead_mutex);
+	if (philo->data->is_dead || philo->data->ate_enough)
+	{
+		if (*l_fork)
+			pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
+		if (*r_fork)
+			pthread_mutex_unlock(&philo->data->forks[philo->id
+				% philo->data->nbr_of_philos]);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->data->dead_mutex);
+	return (0);
 }
