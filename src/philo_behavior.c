748b1227ed1fad392ae9ed7361e50b436eaa76e8
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 16:33:07 -0300

    adjust(philo_behavior): changed to become more util

--- src/philo_behavior.c
+++ src/philo_behavior.c
@@ -20,6 +20,13 @@ int	behavior_prevention(t_philo *philo, unsigned int *left_fork,
 
 void	philo_behavior(t_philo *philo, char *action)
 {
+	pthread_mutex_lock(&philo->data->dead_mutex);
+	if (philo->data->is_dead || philo->data->ate_enough)
+	{
+		pthread_mutex_unlock(&philo->data->dead_mutex);
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->data->dead_mutex);
 	pthread_mutex_lock(&philo->data->print_mutex);
 	printf("Philosopher [%d] %s!\n", philo->id, action);
 	pthread_mutex_unlock(&philo->data->print_mutex);
