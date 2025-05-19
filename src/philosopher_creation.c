0ca3189ad80ce3e7dbfe5643a2d7ac6c6bc3dbab
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 16:20:38 -0300

    change(get_time\safe_unsleep): aux functions removed  from philosopher_creation file

--- src/philosopher_creation.c
+++ src/philosopher_creation.c
@@ -1,33 +1,5 @@
 #include "../philo.h"
 
-long	get_time(void)
-{
-	struct timeval	time;
-
-	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000 + time.tv_usec / 1000);
-}
-
-void	safe_usleep(unsigned long duration, t_philo *philo)
-{
-	long	start;
-
-	start = get_time();
-	while (6)
-	{
-		pthread_mutex_lock(&philo->data->dead_mutex);
-		if (philo->data->is_dead || philo->data->ate_enough)
-		{
-			pthread_mutex_unlock(&philo->data->dead_mutex);
-			return ;
-		}
-		pthread_mutex_unlock(&philo->data->dead_mutex);
-		if (get_time() - start >= duration)
-			return ;
-		usleep(500);
-	}
-}
-
 static void	*philo_create(void *arg)
 {
 	unsigned int	right_fork;
