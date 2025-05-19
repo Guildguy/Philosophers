5c4758040f84a18209f76b2ac8b956304be2b760
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 16:19:55 -0300

    change(get_time\safe_unsleep): aux functions moved to data_handler file

--- src/data_handler.c
+++ src/data_handler.c
@@ -1,5 +1,33 @@
 #include "../philo.h"
 
+long	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+void	safe_usleep(unsigned long duration, t_philo *philo)
+{
+	long	start;
+
+	start = get_time();
+	while (6)
+	{
+		pthread_mutex_lock(&philo->data->dead_mutex);
+		if (philo->data->is_dead || philo->data->ate_enough)
+		{
+			pthread_mutex_unlock(&philo->data->dead_mutex);
+			return ;
+		}
+		pthread_mutex_unlock(&philo->data->dead_mutex);
+		if (get_time() - start >= duration)
+			return ;
+		usleep(500);
+	}
+}
+
 void	free_all(t_data *data)
 {
 	int	i;
