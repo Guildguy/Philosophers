94df7760db5868e63cc65aef0262caa42231b336
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 13:49:36 -0300

    Feat(Create_Fork): Added a new function that is responsible for creating forks

--- src/fork.c
+++ src/fork.c
@@ -1,2 +1,19 @@
 #include "../philo.h"
 
+int	create_fork(t_data *data)
+{
+	int	i;
+
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->nbr_of_philos);
+	if (!data->forks)
+	{
+		printf("Error:failure to allocate memory for fork!\n");
+		return (1);
+	}
+	i = 0;
+	while (i < data->nbr_of_philos)
+	{
+		i++;
+	}
+	return (0);
+}
