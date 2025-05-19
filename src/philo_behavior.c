bca6e886541e7a01fa1d06801d7f4380a01b9591
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 17:04:22 -0300

    feat(think_time): philo think behavior

--- src/philo_behavior.c
+++ src/philo_behavior.c
@@ -18,7 +18,7 @@ int	behavior_prevention(t_philo *philo, unsigned int *left_fork,
 	return (0);
 }
 
-void	philo_behavior(t_philo *philo, char *action)
+int	philo_behavior(t_philo *philo, char *action)
 {
 	pthread_mutex_lock(&philo->data->dead_mutex);
 	if (philo->data->is_dead || philo->data->ate_enough)
@@ -30,14 +30,14 @@ void	philo_behavior(t_philo *philo, char *action)
 	pthread_mutex_lock(&philo->data->print_mutex);
 	printf("Philosopher [%d] %s!\n", philo->id, action);
 	pthread_mutex_unlock(&philo->data->print_mutex);
+	return (0);
 }
 
 int	eat_time(t_philo *philo, unsigned int *left_fork,
 		unsigned int *right_fork)
 {
-	if (behavior_prevention(philo, left_fork, right_fork))
+	if (philo_behavior(philo, "is eating!"))
 		return (1);
-	philo_behavior(philo, "is eating!");
 	philo->last_meal = get_time();
 	philo->meals++;
 	safe_usleep(philo->data->time_to_eat, philo);
@@ -47,9 +47,14 @@ int	eat_time(t_philo *philo, unsigned int *left_fork,
 int	sleep_time(t_philo *philo, unsigned int *left_fork,
 		unsigned int *right_fork)
 {
-	if (behavior_prevention(philo, left_fork, right_fork))
-		return (1) ;
-	philo_behavior(philo, "is sleeping!");
+	if (philo_behavior(philo, "is sleeping!"))
+		return (1);
 	safe_usleep(philo->data->time_to_sleep, philo);
 	return (0);
+}
+
+int	think_time(t_philo *philo, unsigned int *left_fork,
+		unsigned int *right_fork)
+{
+	return (philo_behavior(philo, "is thinking!"));
 }
