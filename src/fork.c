4668598b698a20913145f30c12c5efa305fd9248
Guildguy <fabiolljunior23@gmail.com>
2025-05-19 19:52:54 -0300

    change(cleanup_fork): added function that cleans up the fork in philo_create()

--- src/fork.c
+++ src/fork.c
@@ -40,33 +40,6 @@ static int	odd_philo(t_philo *philo, unsigned int *left_fork,
 	return (0);
 }
 
-int	create_fork(t_data *data)
-{
-	int	i;
-
-	data->forks = malloc(sizeof(pthread_mutex_t) * data->nbr_of_philos);
-	if (!data->forks)
-	{
-		printf("Error:failure to allocate memory for fork!\n");
-		return (1);
-	}
-	i = 0;
-	while (i < data->nbr_of_philos)
-	{
-		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
-		{
-			while (--i > 0)
-				pthread_mutex_destroy(&data->forks[i]);
-			free(data->forks);
-			data->forks = NULL;
-			printf("Error: failure to initialize forks!\n");
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
 int	take_fork(t_philo *philo, unsigned int *left_fork,
 	unsigned int *right_fork)
 {
@@ -75,6 +48,7 @@ int	take_fork(t_philo *philo, unsigned int *left_fork,
 	else
 		return(odd_philo(philo, left_fork, right_fork));
 }
+
 int	release_fork(t_philo *philo, unsigned int *left_fork,
 		unsigned int *right_fork)
 {
@@ -83,7 +57,7 @@ int	release_fork(t_philo *philo, unsigned int *left_fork,
 		if (behavior_prevention(philo, left_fork, right_fork))
 			return (1);
 		pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
-		if (philo_behavior(philo, "released left fork!"))
+		if (philo_behavior(philo, "released the left fork!"))
 			return (1);
 		*left_fork = 0;
 	}
@@ -93,9 +67,19 @@ int	release_fork(t_philo *philo, unsigned int *left_fork,
 			return (1);
 		pthread_mutex_unlock(&philo->data->forks[philo->id
 			% philo->data->nbr_of_philos]);
-		if (philo_behavior(philo, "released right fork!"))
+		if (philo_behavior(philo, "released the right fork!"))
 			return (1);
 		*right_fork = 0;
 	}
 	return (0);
+}
+
+void	cleanup_fork(t_philo *philo, unsigned int *left_fork,
+	unsigned int *right_fork)
+{
+if (*left_fork) 
+pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
+if (*right_fork)
+pthread_mutex_unlock(&philo->data->forks[philo->id
+	% philo->data->nbr_of_philos]);
 }
