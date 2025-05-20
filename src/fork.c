6dbab156a64e4805c3531c9ef8aae23aa450f926
Guildguy <fabiolljunior23@gmail.com>
2025-05-20 12:22:34 -0300

    update(fork): updated to treat the one philo case and ortography errors!

--- src/fork.c
+++ src/fork.c
@@ -8,14 +8,16 @@ static int	pair_philo(t_philo *philo, unsigned int *left_fork,
 	*left_fork = 1;
 	if (behavior_prevention(philo, left_fork, right_fork))
 		return (1);
-	if (philo_behavior(philo, "has taken a left fork!"))
+	if (philo_behavior(philo, "has taken a left fork"))
+		return (1);
+	if (philo->data->nbr_of_philos = 1)
 		return (1);
 	pthread_mutex_lock(&philo->data->forks[philo->id
 		% philo->data->nbr_of_philos]);
 	*right_fork = 1;
 	if (behavior_prevention(philo, left_fork, right_fork))
 		return (1);
-	if (philo_behavior(philo, "has taken a right fork!"))
+	if (philo_behavior(philo, "has taken a right fork"))
 		return (1);
 	return (0);
 }
@@ -28,14 +30,16 @@ static int	odd_philo(t_philo *philo, unsigned int *left_fork,
 	*right_fork = 1;
 	if (behavior_prevention(philo, left_fork, right_fork))
 		return (1);
-	if (philo_behavior(philo, "has taken a right fork!"))
+	if (philo_behavior(philo, "has taken a right fork"))
 		return (1);
+	if (philo->data->nbr_of_philos == 1)
+		rerurn (1);
 	pthread_mutex_lock
 		(&philo->data->forks[philo->id - 1]);
 	*left_fork = 1;
 	if (behavior_prevention(philo, left_fork, right_fork))
 		return (1);
-	if (philo_behavior(philo, "has taken a left fork!"))
+	if (philo_behavior(philo, "has taken a left fork"))
 		return (1);
 	return (0);
 }
@@ -57,7 +61,7 @@ int	release_fork(t_philo *philo, unsigned int *left_fork,
 		if (behavior_prevention(philo, left_fork, right_fork))
 			return (1);
 		pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
-		if (philo_behavior(philo, "released the left fork!"))
+		if (philo_behavior(philo, "released the left fork"))
 			return (1);
 		*left_fork = 0;
 	}
@@ -67,7 +71,7 @@ int	release_fork(t_philo *philo, unsigned int *left_fork,
 			return (1);
 		pthread_mutex_unlock(&philo->data->forks[philo->id
 			% philo->data->nbr_of_philos]);
-		if (philo_behavior(philo, "released the right fork!"))
+		if (philo_behavior(philo, "released the right fork"))
 			return (1);
 		*right_fork = 0;
 	}
