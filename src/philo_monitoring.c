8da05cf87781bb5d9173a331e6ac72eb93417632
Guildguy <fabiolljunior23@gmail.com>
2025-05-20 11:56:23 -0300

    feat(philo_status): added philo_status to verify if the philo is alive/dead

--- src/philo_monitoring.c
+++ src/philo_monitoring.c
@@ -1,58 +1,25 @@
 #include "../philo.h"
 
-void	*monitor_routine(void *arg)
+static int	philo_status(t_data *data, unsigned int i,
+	unsigned int *meals_done)
 {
-	unsigned int	i;
-	unsigned int	j;
-	t_data			*data;
-
-	data = (t_data *)arg;
-	while (6)
+	pthread_mutex_lock(&data->dead_mutex);
+	if (data->is_dead || data->ate_enough)
 	{
-		i = 0;
-		while (i < data->nbr_of_philos)
-		{
-			pthread_mutex_lock(&data->dead_mutex);
-			if (data->is_dead || data->ate_enough)
-			{
-				pthread_mutex_unlock(&data->dead_mutex);
-				return (NULL);
-			}		
-			if (data->nbr_of_meals > 0)
-			{
-				j = 0;
-				while (j < data->nbr_of_philos)
-				{
-					if (data->philos[j].meals < data->nbr_of_meals)
-						break ;
-					j++;
-				}
-				if (j == data->nbr_of_philos)
-				{
-					data->ate_enough = 1;
-					pthread_mutex_lock(&data->print_mutex);
-					printf("philosophers ate %u times!\n",
-						data->nbr_of_meals);
-					pthread_mutex_unlock(&data->print_mutex);
-					pthread_mutex_unlock(&data->dead_mutex);
-					return (NULL);
-				}
-			}
-			if (get_time() - data->philos[i].last_meal >= data->time_to_die)
-			{
-				data->is_dead = 1;
-				pthread_mutex_lock(&data->print_mutex);
-				printf("philosopher [%d] is dead!\n", data->philos[i].id);
-				pthread_mutex_unlock(&data->print_mutex);
-				pthread_mutex_unlock(&data->dead_mutex);
-				return (NULL);
-			}
-			pthread_mutex_unlock(&data->dead_mutex);
-			i++;
-		}
-		usleep(500);
+		pthread_mutex_unlock(&data->dead_mutex);
+		return (1);
 	}
-	return (NULL);
+	if (get_time() - data->philos[i].last_meal >= data->time_to_die)
+	{
+		data->is_dead = 1;
+		pthread_mutex_unlock(&data->dead_mutex);
+		philo_behavior(&data->philos[i], "died");
+		return (1);
+	}
+	if (*meals_done && data->philos[i].meals < data->nbr_of_meals)
+		*meals_done = 0;
+	pthread_mutex_unlock(&data->dead_mutex);
+	return (0);
 }
 
 void	*monitor_routine(void *arg)
@@ -61,13 +28,24 @@ void	*monitor_routine(void *arg)
 	unsigned int	i;
 	unsigned int	meals_done;
 
-	i = 0;
-	meals_done = data->nbr_of_meals > 0;
-	data = 	(t_data *)arg;
-	while (i < data->nbr_of_philos)
-	{
-		pthread_mutex_lock(&data->dead_mutex);
-		pthread_mutex_unlock(&data->dead_mutex);
-		i++;
-	}
+	data = (t_data *)arg;
+    while (6)
+    {
+        i = 0;
+        meals_done = data->nbr_of_meals > 0;
+        while (i < data->nbr_of_philos)
+        {
+            if (philo_status(data, i, &meals_done))
+                return (NULL);
+            i++;
+        }
+        if (meals_done)
+        {
+            data->ate_enough = 1;
+            philo_behavior(&data->philos[0], "ate_enough");
+            return (NULL);
+        }
+        usleep(500);
+    }
+    return (NULL);
 }
